bf78e01b654ef8faabb931ae8f26c50e3dbc97d7
Noo7-Windows8LenovoLaptop <8bitbug@gmail.com>
2014-11-22 20:27:28 +0100

    First commit implementing makeCacheMatrix and cacheSolve, tested and working correctly

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to create Matrices and access the inverse of Matrices
+## while caching the inverse
 
-## Write a short comment describing this function
+
+## This function return a special matrix containing
+## an element storing its own inverse, plus a few functions
+## to set and get the data it contains
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## initialize the inverse to null
+    inv <- NULL
+    
+    ## set the data to be equal to y
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    ## get the contents of the matrix
+    get <- function() x
+    ## set the Inverse of the matrix
+    setInv <- function(inverse) inv <<- inverse
+    ## get the inverse of the matrix
+    getInv <- function() inv
+    
+    #return a list conatining accessors to the data
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of a matrix x
+## if the inverse of the matrix has alredy been calculated
+## and its contents havent been changed since last calculation
+## then the cached answer to the inverse will be returned
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+    ## get the Inverse from x which is cached in x
+    inv <- x$getInv()
+    ## check if retrievedvalue is set, if so return it
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    ## retrived cached inverse is not set, so calculate it, set it, return it
+    data <- x$get()
+    inv <- solve(data)
+    x$setInv(inv)
+    inv
 }
